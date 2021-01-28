fe16a04ebbbbc037b6849a3fe1aed385c565fe8d
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-28 11:40:00 -0500

    Initial Commit

---
+++ Shared/LCS_EatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCS_EatsApp.swift
+//  Shared
+//
+//  Created by Doggett, Scott on 2021-01-28.
+//
+
+import SwiftUI
+
+@main
+struct LCS_EatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
