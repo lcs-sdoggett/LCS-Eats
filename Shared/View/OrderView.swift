6f9667e0385625a0877976c76eb94946ee08ad70
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-28 12:25:23 -0500

    Very basic static view of Order review created

--- Shared/View/OrderView.swift
+++ Shared/View/OrderView.swift
@@ -9,7 +9,73 @@ import SwiftUI
 
 struct OrderView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            // Name text
+            HStack {
+                Text("Name: Scott D")
+                    .padding(.leading)
+                Spacer()
+            }
+            
+            // Phone#/Email text
+            HStack {
+                Text("Phone#/Email: (416)-992-9943")
+                    .padding([.leading, .top])
+                Spacer()
+            }
+            
+            // Items text
+            HStack {
+                Text("Items:")
+                    .padding([.leading, .top])
+                Spacer()
+            }
+            
+            // Item 1 text
+            HStack {
+                Text("Burger")
+                    .padding([.leading, .top])
+                Spacer()
+                Text("$2.53")
+                    .padding([.trailing, .top])
+            }
+            
+            // Item 2 text
+            HStack {
+                Text("Drink")
+                    .padding([.leading, .top])
+                Spacer()
+                Text("$5.32")
+                    .padding([.trailing, .top])
+            }
+            
+            // Tax tect
+            HStack {
+                Text("Tax")
+                    .padding([.leading, .top])
+                Spacer()
+                Text("$2.30")
+                    .padding()
+            }
+            
+            // Delivery text
+            HStack {
+                Text("Delivery")
+                    .padding(.leading)
+                Spacer()
+                Text("$2.32")
+                    .padding()
+            }
+            
+            // Button
+            Button("Place Order Total: $8.34") {
+                
+            }
+            .padding(.top)
+            
+            .navigationTitle("Order Summary")
+        }
     }
 }
 
