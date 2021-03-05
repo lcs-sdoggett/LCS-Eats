2ec2c609f882c829562d982685cb197c9d33c30a
Scott Doggett <sdoggett@lcs.on.ca>
2021-03-04 20:17:33 -0500

    Got rid of extra mcdonald's and tims menus that are not in use anymore

--- Shared/Model/Item.swift
+++ Shared/Model/Item.swift
@@ -15,18 +15,3 @@ struct Item: Identifiable {
     let imageName: String
     
 }
-
-let mcdonaldsMenu = [
-    Item(name: "Bic Mac", price: 6.19, imageName: "MDbigmac"),
-    Item(name: "Double Big Mac", price: 8.19, imageName: "MDbigmac"),
-    Item(name: "Quarter Pounder With Cheese", price: 6.19, imageName: "MDquarterpounderwithcheese"),
-    Item(name: "Double Quarter Pounder With Cheese", price: 8.19, imageName: "MDdoublequarterpounderwithcheese")
-]
-
-let timsMenu = [
-    Item(name: "Donut", price: 0.99, imageName: "THdonut"),
-    Item(name: "Muffin", price: 1.49, imageName: "THmuffin"),
-    Item(name: "Danish", price: 1.49, imageName: "THdanish")
-]
-
-
