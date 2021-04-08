4f0d959dec81b238530e4ea8af15372d2f2ceead
Scott Doggett <sdoggett@lcs.on.ca>
2021-04-08 14:19:47 -0400

    Email sender file added

---
+++ Shared/Model/EmailSender.swift
@@ -0,0 +1,54 @@
+//
+//  EmailSender.swift
+//  LCS Eats
+//
+//  Created by Doggett, Scott on 2021-04-08.
+//
+
+import Foundation
+import MessageUI
+
+enum EmailSendError: Error {
+  case noMailClientConfigured
+}
+
+class EmailHelper: NSObject, MFMailComposeViewControllerDelegate {
+    
+    public static let shared = EmailHelper()
+    
+    private override init() {
+        //
+    }
+    
+    func sendEmail(subject: String, body: String, isHTML: Bool = false) throws {
+        
+        if !MFMailComposeViewController.canSendMail() {
+            
+            print("No mail account found")
+            throw EmailSendError.noMailClientConfigured
+            
+        }
+        
+        let picker = MFMailComposeViewController()
+        
+        picker.setSubject(subject)
+        picker.setMessageBody(body, isHTML: isHTML)
+        picker.setToRecipients(["sdoggett@lcs.on.ca"])
+        picker.mailComposeDelegate = self
+        
+        EmailHelper.getRootViewController()?.present(picker, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        EmailHelper.getRootViewController()?.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    static func getRootViewController() -> UIViewController? {
+//        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController
+        
+//         OR If you use SwiftUI 2.0 based WindowGroup try this one
+         UIApplication.shared.windows.first?.rootViewController
+    }
+}
